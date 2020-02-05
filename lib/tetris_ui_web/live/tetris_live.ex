73c3d7265c021504fb997a123fdd33e26a26651b
ryanyogan <ryanyogan@gmail.com>
2020-02-05 12:26:07 -0800

    render out a basic SVG via LiveView

--- lib/tetris_ui_web/live/tetris_live.ex
+++ lib/tetris_ui_web/live/tetris_live.ex
@@ -4,8 +4,10 @@ defmodule TetrisUiWeb.TetrisLive do
 
   alias Tetris.Brick
 
-  @spec mount(any, any, Phoenix.LiveView.Socket.t()) :: {:ok, any}
-  def mount(_params, _session, socket) do
+  @box_width 20
+  @box_height 20
+
+  def mount(_session, socket) do
     {
       :ok,
       assign(socket,
@@ -14,10 +16,65 @@ defmodule TetrisUiWeb.TetrisLive do
     }
   end
 
-  @spec render(any) :: Phoenix.LiveView.Rendered.t()
   def render(assigns) do
     ~L"""
-    <pre><%= @tetromino %></pre>
+    <div>
+      <%= raw svg_head() %>
+      <%= raw box({1, 1}, :grey) %>
+      <%= raw svg_foot() %>
+    </div>
     """
   end
+
+  def svg_head() do
+    """
+    <svg
+      version="1.0"
+      id="Layer_1"
+      xmlns="http://www.w3.org/2000/svg"
+      xmlns:xlink="http://w3.org/1999/xlink"
+      width="200" height="400"
+      viewBox="0 0 200 400"
+      xml:space="preserve">
+    """
+  end
+
+  def svg_foot(), do: "</svg>"
+
+  def box(point, color) do
+    """
+      #{square(point, shades(color).dark)}
+      #{triangle(point, shades(color).light)}
+    """
+  end
+
+  def square(point, shade) do
+    {x, y} = to_pixels(point)
+
+    """
+    <rect
+      x="#{x + 1}" y="#{y + 1}"
+      style="fill:##{shade}"
+      width="#{@box_width - 2}" height="#{@box_height - 2}" />
+    """
+  end
+
+  def triangle(point, shade) do
+    {x, y} = to_pixels(point)
+    {w, h} = {@box_width, @box_height}
+
+    """
+    <polyline
+      style="fill:##{shade}"
+      points="#{x + 1}, #{y + 1} #{x + w}, #{y + 1} #{x + w}, #{y + h}" />
+    """
+  end
+
+  defp shades(:red), do: %{light: "DB7160", dark: "AB574B"}
+  defp shades(:blue), do: %{light: "83C1C8", dark: "66969C"}
+  defp shades(:green), do: %{light: "8BBF57", dark: "769359"}
+  defp shades(:orange), do: %{light: "CB8E4E", dark: "AC7842"}
+  defp shades(:grey), do: %{light: "A1A09E", dark: "7F7F7E"}
+
+  defp to_pixels({x, y}), do: {x * @box_width, y * @box_height}
 end
