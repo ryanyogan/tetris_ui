9afbf7265ec9721fd17b0f8e1a983a1da997b005
ryanyogan <ryanyogan@gmail.com>
2020-02-05 11:14:29 -0800

    Rolled back to working npm deps, display a simple tetromino via live view

--- lib/tetris_ui_web/live/tetris_live.ex
+++ lib/tetris_ui_web/live/tetris_live.ex
@@ -2,12 +2,14 @@ defmodule TetrisUiWeb.TetrisLive do
   use Phoenix.LiveView
   use Phoenix.HTML, only: [raw: 1]
 
-  def mount(_session, socket) do
+  alias Tetris.Brick
+
+  @spec mount(any, any, Phoenix.LiveView.Socket.t()) :: {:ok, any}
+  def mount(_params, _session, socket) do
     {
       :ok,
       assign(socket,
-        hello: :world,
-        name: :joe
+        tetromino: Brick.new_random() |> Brick.to_string()
       )
     }
   end
@@ -15,7 +17,7 @@ defmodule TetrisUiWeb.TetrisLive do
   @spec render(any) :: Phoenix.LiveView.Rendered.t()
   def render(assigns) do
     ~L"""
-      <h1>Hello, <%= @hello %> <%= @name %></h1>
+    <pre><%= @tetromino %></pre>
     """
   end
 end
