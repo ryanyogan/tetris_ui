efc8be98598d88b0da666de6211b47c39c9db674
ryanyogan <ryanyogan@gmail.com>
2020-02-05 17:35:33 -0800

    start of a cleaner api to use via LiveView

--- tetris/lib/tetris.ex
+++ tetris/lib/tetris.ex
@@ -1,18 +1,27 @@
 defmodule Tetris do
   @moduledoc """
-  Documentation for Tetris.
+  Interface layer for Tetris game.
   """
 
-  @doc """
-  Hello world.
+  alias Tetris.{Brick, Gutter, Points}
 
-  ## Examples
-
-      iex> Tetris.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def prepare(brick) do
+    brick
+    |> Brick.prepare()
+    |> Points.move_to_location(brick.location)
   end
+
+  def try_move(brick, gutter, f) do
+    new_brick = f.(brick)
+
+    if Gutter.collides?(gutter, prepare(new_brick)) do
+      brick
+    else
+      new_brick
+    end
+  end
+
+  def try_left(brick, gutter), do: try_move(brick, gutter, &Brick.left/1)
+  def try_right(brick, gutter), do: try_move(brick, gutter, &Brick.right/1)
+  def try_spin_90(brick, gutter), do: try_move(brick, gutter, &Brick.spin_90/1)
 end
--- tetris/test/tetris_test.exs
+++ tetris/test/tetris_test.exs
@@ -1,8 +1,25 @@
 defmodule TetrisTest do
   use ExUnit.Case
-  doctest Tetris
 
-  test "greets the world" do
-    assert Tetris.hello() == :world
+  import Tetris
+  alias Tetris.Brick
+
+  test "try to move right, success" do
+    brick = Brick.new(location: {5, 1})
+    gutter = %{}
+
+    expected = brick |> Brick.right()
+    actual = try_right(brick, gutter)
+
+    assert actual == expected
+  end
+
+  test "try to move right, failure returns original brick" do
+    brick = Brick.new(location: {8, 1})
+    gutter = %{}
+
+    actual = try_right(brick, gutter)
+
+    assert actual == brick
   end
 end
